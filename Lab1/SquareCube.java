052e772e93b6ed1d41c26421379ec2cb2293a4ae
sohanrane20 <sohanrane20@gmail.com>
2026-01-13 13:19:48 +0530

    Update SquareCube.java

--- Lab1/SquareCube.java
+++ Lab1/SquareCube.java
@@ -1,10 +1,9 @@
 package lab1;
 class Table
 {
-	int i;
 	void writeTable()
 	{
-		for (i = 1; i <= 10; i++)
+		for (int i = 1; i <= 10; i++)
 		{
 			System.out.println(i + " " + i * i + " " + i * i * i);
 		}
