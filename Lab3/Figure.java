a4e2ca894a259a08c12290ef6bf98af0cac91974
sohanrane20 <sohanrane20@gmail.com>
2026-01-19 20:30:14 +0530

    Delete Lab3 directory

--- Lab3/Figure.java
+++
@@ -1,9 +0,0 @@
-package lab3.figpackage;
-abstract class Figure
-{
-	double pi = 3.142;
-	public abstract void calcArea();
-	public abstract void calcVol(); 
-	public abstract void dispArea();
-	public abstract void dispVol();
-}
