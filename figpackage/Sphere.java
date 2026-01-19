934ea3c000ce8e82a2d7a5126767092553d0f7a1
sohanrane20 <sohanrane20@gmail.com>
2026-01-19 20:29:17 +0530

    Add files via upload

---
+++ figpackage/Sphere.java
@@ -0,0 +1,21 @@
+package lab3.figpackage;
+class Sphere extends Figure
+{
+	double r, a, v;
+	public void calcArea()
+	{
+		a = 4 * pi * r * r;
+	}
+	public void calcVol()
+	{
+		v = 4 * pi * r * r * r / 3;
+	}
+	public void dispArea()
+	{
+		System.out.println("Area of sphere is " + a);
+	}
+	public void dispVol()
+	{
+		System.out.println("Volume of sphere is " + v);
+	}
+}
